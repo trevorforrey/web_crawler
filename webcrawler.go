442da3534ef38e6a83b493f7bd71965003555484
trevorforrey <trevor4e@gmail.com>
2017-08-10 19:29:20 -0700

    Sync Recursion Image Crawling Works

--- webcrawler.go
+++ webcrawler.go
@@ -5,135 +5,172 @@
 // The web crawler has been modified to take input from a running Go server
 // and display images found from crawling
 
+// TODO Clean and perfect this version. Research error handling, formatting, and Benchmarking
+
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "golang.org/x/net/html"
-  "html/template"
+	"fmt"
+	"golang.org/x/net/html"
+	"html/template"
+	"net/http"
+	"strings"
 )
 
 type PageVars struct {
-  TotalCount int
-  Links []string
-  //TotalTime float64
-	//URLsVisisted int
+	LinkCountTotal int
+	Links          []string
+	Images         []string
+	//TotalTime float64
+	ImageCountTotal int
 }
 
 var resultUrls []string
-
+var resultImgs []string
+var emptyList []string
 
 func main() {
-  http.HandleFunc("/", home)
-  http.HandleFunc("/crawl", search)
-  http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", home)
+	http.HandleFunc("/crawl", search)
+	http.ListenAndServe(":8080", nil)
 }
 
-
 func home(writer http.ResponseWriter, r *http.Request) {
-    t, _ := template.ParseFiles("home.html")
-    t.Execute(writer, nil)
+	t, _ := template.ParseFiles("home.html")
+	t.Execute(writer, nil)
 }
 
 func search(writer http.ResponseWriter, r *http.Request) {
-  //Create Page Vars variable
-  var resultVars PageVars
+
+	var resultVars PageVars
 
 	//Make sure http request is NOT a GET
 	//If it is, return an error to the user
 
-  if err := r.ParseForm(); err != nil {
-    fmt.Println("ERROR READING FROM FORM")
-  }
+	if err := r.ParseForm(); err != nil {
+		fmt.Println("ERROR READING FROM FORM")
+	}
 
-  // seen := make(map[string]bool)
-  var baseUrls []string
-  fmt.Print("starting urls")
+	var baseUrls []string
 	baseUrls = append(baseUrls, r.PostFormValue("baseURLs"))
-  for _, url := range baseUrls {
-    fmt.Println(url)
-  }
-  fmt.Print("ending urls")
+
 	links := crawl(baseUrls, 2)
 
-  resultVars.TotalCount = len(links)
-  resultVars.Links = links
+	resultVars.LinkCountTotal = len(links)
+	resultVars.Links = links
+	resultVars.ImageCountTotal = len(resultImgs)
+	resultVars.Images = resultImgs
 
-  t, _ := template.ParseFiles("results.html")
-  t.Execute(writer, resultVars)
+	t, _ := template.ParseFiles("results.html")
+	t.Execute(writer, resultVars)
 }
 
-
 func crawl(urls []string, depth int) []string {
-  var emptyList []string
-  depth--
+
+	depth--
 	if depth == 0 {
-    fmt.Println("Reached depth of zero")
+		fmt.Println("Reached depth of zero")
 		return emptyList
 	}
-	for _, link := range urls {
-		// if seen(link) {
-		// 	continue
-		// }
-    // seen[link] = true
-    fmt.Println(link)
-    newUrls, err := extract(link)
-    if err != nil {
-      fmt.Print(err)
-    }
 
-		resultUrls = append(newUrls, crawl(newUrls, depth)...) // returns string[] of links
+	for _, link := range urls {
+		fmt.Println(link)
+		pageImgs, err := extractImgs(link)
+
+		if err != nil {
+			fmt.Print(err)
+			continue
+		}
+
+		resultImgs = append(resultImgs, pageImgs...)
+		newUrls, err := extractLinks(link)
+
+		if err != nil {
+			fmt.Print(err)
+			continue
+		}
+		resultUrls = append(newUrls, crawl(newUrls, depth)...)
 	}
 	return resultUrls
 }
 
-
 // Extracts all urls from a web page
-func extract(url string) (contents []string, err error) {
+func extractLinks(url string) (links []string, err error) {
 
-  resp, err := http.Get(url)
-  if err != nil {
-    return nil, err
-  }
-  if resp.StatusCode != http.StatusOK {
-      resp.Body.Close()
-      return nil, fmt.Errorf("Error getting: %s | %s", url, resp.Status)
-  }
-  page, err := html.Parse(resp.Body) // returns root *htmlNode
-  resp.Body.Close()
-  if err != nil {
-    fmt.Errorf("Error parsing html:%s,  %v", url, err)
-  }
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("Error getting: %s | %s", url, resp.Status)
+	}
+	page, err := html.Parse(resp.Body) // returns root *htmlNode
+	resp.Body.Close()
+	if err != nil {
+		fmt.Errorf("Error parsing html:%s,  %v", url, err)
+		return nil, err
+	}
 
-  var links []string
-  var visitNode func(node *html.Node)
-  visitNode = func(node *html.Node) {
-    if node.Type == html.ElementNode && node.Data == "a" {
-      for _, a := range node.Attr {
-        if a.Key == "href" {
-          link, err := resp.Request.URL.Parse(a.Val)
-          if err != nil { // only accept valid urls
-            continue;
-          }
-          links = append(links, link.String())
-        }
-      }
-    }
-  }
-  forEveryNode(page, visitNode, nil)
-  return links, err
+	var visitNode func(node *html.Node)
+	visitNode = func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "a" {
+			for _, a := range node.Attr {
+				if a.Key == "href" {
+					link, err := resp.Request.URL.Parse(a.Val)
+					if err != nil { // only accept valid urls
+						continue
+					}
+					links = append(links, link.String())
+				}
+			}
+		}
+	}
+	forEveryNode(page, visitNode, nil)
+	return links, err
 }
 
+// Extracts all imgs from a web page
+func extractImgs(url string) (images []string, err error) {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("Error getting: %s | %s", url, resp.Status)
+	}
+	page, err := html.Parse(resp.Body) // returns root *htmlNode
+	resp.Body.Close()
+	if err != nil {
+		fmt.Errorf("Error parsing html:%s,  %v", url, err)
+		return nil, err
+	}
+
+	var visitNode func(node *html.Node)
+	visitNode = func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "img" {
+			for _, a := range node.Attr {
+				if strings.HasPrefix(a.Val, "https://") {
+					fmt.Println(a.Val)
+					images = append(images, a.Val)
+				}
+			}
+		}
+	}
+	forEveryNode(page, visitNode, nil)
+	return images, err
+}
 
 func forEveryNode(node *html.Node, pre, post func(n *html.Node)) {
-  if pre != nil {
-    pre(node)
-  }
-  for child := node.FirstChild; child != nil; child = child.NextSibling {
-    forEveryNode(child, pre, post)
-  }
-  if post != nil {
-    post(node)
-  }
+	if pre != nil {
+		pre(node)
+	}
+	for child := node.FirstChild; child != nil; child = child.NextSibling {
+		forEveryNode(child, pre, post)
+	}
+	if post != nil {
+		post(node)
+	}
 }
